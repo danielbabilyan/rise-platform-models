a6a1f85690f40e8133beed533081a6e38d802f10
Daniel Babilyan <danielbab@wix.com>
2022-08-16 14:48:16 +0300

    userScopes

--- rise-platform-auth/src/requiresAuth.ts
+++ rise-platform-auth/src/requiresAuth.ts
@@ -8,55 +8,61 @@ interface JwtPayload {
   user_id: string;
 }
 
-export async function requiresAuth(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
-  try {
-    const token = getTokenFromReq(req);
+export function requiresAuth({ scope }: { scope?: string } = {}) {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = getTokenFromReq(req);
 
-    if (!token) {
-      throw new Error("Token not found");
-    }
+      if (!token) {
+        throw new Error("Token not found");
+      }
 
-    const { profile_id, user_id } = jwt.verify(
-      token,
-      `${process.env.JWT_SECRET}`
-    ) as JwtPayload;
+      const { profile_id, user_id } = jwt.verify(
+        token,
+        `${process.env.JWT_SECRET}`
+      ) as JwtPayload;
 
-    const profile = await global.db_models.profile.findUnique({
-      where: { id: profile_id },
-      include: {
-        users: {
-          include: {
-            account: true,
-          },
-        },
-      },
-    });
-
-    if (!profile) throw new Error("profile not found");
-    req.profile = profile;
-
-    if (user_id) {
-      const user = await global.db_models.user.findUnique({
-        where: { id: user_id },
+      const profile = await global.db_models.profile.findUnique({
+        where: { id: profile_id },
         include: {
-          profile: true,
-          account: true,
-          roles: true,
+          users: {
+            include: {
+              account: true,
+            },
+          },
         },
       });
 
-      if (!user) throw new Error("user not found");
-      req.user = user;
-    }
+      if (!profile) throw new Error("profile not found");
+      req.profile = profile;
 
-    next();
-  } catch (error) {
-    console.error(error);
-    res.clearCookie("jwt");
-    return res.sendStatus(401);
-  }
+      if (user_id) {
+        const user = await global.db_models.user.findUnique({
+          where: { id: user_id },
+          include: {
+            profile: true,
+            account: true,
+            roles: true,
+          },
+        });
+
+        if (!user) throw new Error("user not found");
+
+        if (scope) {
+          const userScopes = user.roles.flatMap(
+            (role) => role.scopes
+          ) as string[];
+
+          if (!userScopes.includes(scope)) throw new Error("scope not found");
+        }
+        req.user = user;
+      }
+
+      next();
+    } catch (error) {
+      console.error(error);
+      res.clearCookie("jwt");
+      return res.sendStatus(401);
+    }
+  };
 }
