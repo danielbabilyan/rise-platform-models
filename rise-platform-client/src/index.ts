b2e23689e980a7375a467ee8c6b0e20e034030fd
Daniel Babilyan <danielbab@wix.com>
2022-08-18 13:39:34 +0300

    connect

--- rise-platform-client/src/index.ts
+++ rise-platform-client/src/index.ts
@@ -9,14 +9,20 @@ export class RiseClient {
     this.app_secret = app_secret;
   }
 
-  async connect() {
+  async connect({
+    callback_url,
+    scopes,
+  }: {
+    callback_url: string;
+    scopes: string[];
+  }) {
     const { data } = await axios.post(
       "http://localhost:5000/account/apps/connect",
       {
         app_id: this.app_id,
         app_secret: this.app_secret,
-        callback_url: "http://localhost:5001/connect/rise/callback",
-        scopes: ["read:gift_cards", "write:gift_cards"],
+        callback_url,
+        scopes,
       }
     );
     const connect_url: string = data.connect_url;
